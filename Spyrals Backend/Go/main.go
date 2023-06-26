067d3b23909b9ba7449d3e7b1bfb53f035bc1219
1twalters01 <1twalters01@gmail.com>
2023-06-26 16:36:22 +0100

    initialise go repl

--- Spyrals Backend/Go/main.go
+++ Spyrals Backend/Go/main.go
@@ -1,1 +1,21 @@
 // main.go
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"go/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the CU lang's programming language in go!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
